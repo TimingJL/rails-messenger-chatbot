38a5034220852052ff39c3a2f8a255473317e4f4
TimingJL <eefozeo@gmail.com>
2017-09-02 13:14:56 +0800

    init

--- bot.rb
+++ bot.rb
@@ -8,11 +8,49 @@ end
 
 include Facebook::Messenger
 
+# 2
 Bot.on :message do |message|
-  Bot.deliver({
+  puts "Received #{message.text} from #{message.sender}"
+
+  Bot.deliver(
     recipient: message.sender,
     message: {
-      text: message.text
+      text: "Oh Hello, would you like to see Hello world in which language?"
     }
-  }, access_token: ENV["ACCESS_TOKEN"])
+  )
+
+  Bot.deliver(
+    recipient: message.sender,
+    message: {
+      attachment: {
+        type: 'template',
+        payload: {
+          template_type: 'button',
+          text: 'What service would you like to log in with?',
+          buttons: [
+            { type: 'postback', title: 'French', payload: 'FRENCH' },
+            { type: 'postback', title: 'Italian', payload: 'ITALIAN' }
+          ]
+        }
+      }
+    }
+  )
+end
+
+# 3
+Bot.on :postback  do |postback|
+
+  case postback.payload
+  when 'FRENCH'
+    text = "Bonjour le monde 🇫🇷"
+  when 'INTALIAN'
+    text = "Ciao mondo 🇮🇹"
+  end
+
+  Bot.deliver(
+    recipient: postback.sender,
+    message: {
+      text: text
+    }
+  )
 end
