371d377645a9b81fcda9f0a83ee9e69d1531ae60
TimingJL <eefozeo@gmail.com>
2017-09-02 13:13:04 +0800

    init

--- bot.rb
+++ bot.rb
@@ -8,49 +8,11 @@ end
 
 include Facebook::Messenger
 
-# 2
 Bot.on :message do |message|
-  puts "Received #{message.text} from #{message.sender}"
-
-  Bot.deliver(
+  Bot.deliver({
     recipient: message.sender,
     message: {
-      text: "Oh Hello, would you like to see Hello world in which language?"
+      text: message.text
     }
-  )
-
-  Bot.deliver(
-    recipient: message.sender,
-    message: {
-      attachment: {
-        type: 'template',
-        payload: {
-          template_type: 'button',
-          text: 'What service would you like to log in with?',
-          buttons: [
-            { type: 'postback', title: 'French', payload: 'FRENCH' },
-            { type: 'postback', title: 'Italian', payload: 'ITALIAN' }
-          ]
-        }
-      }
-    }
-  )
-end
-
-# 3
-Bot.on :postback  do |postback|
-
-  case postback.payload
-  when 'FRENCH'
-    text = "Bonjour le monde 🇫🇷"
-  when 'INTALIAN'
-    text = "Ciao mondo 🇮🇹"
-  end
-
-  Bot.deliver(
-    recipient: postback.sender,
-    message: {
-      text: text
-    }
-  )
+  }, access_token: ENV["ACCESS_TOKEN"])
 end
